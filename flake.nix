cab8b1d678004329caee55fadd9936aa58bffe39
Comamoca <comamoca.dev@gmail.com>
2025-04-21 01:05:35 +0900

    :sparkles: (flake.nix, flake.lock) Flakeファイルを追加

---
+++ flake.nix
@@ -0,0 +1,100 @@
+{
+  description = "A basic flake to with flake-parts";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+    git-hooks-nix.url = "github:cachix/git-hooks.nix";
+    devenv.url = "github:cachix/devenv";
+    nixpkgs-python.url = "github:cachix/nixpkgs-python";
+    nixpkgs-python.inputs = { nixpkgs.follows = "nixpkgs"; };
+  };
+
+  outputs =
+    inputs@{
+      self,
+      systems,
+      nixpkgs,
+      flake-parts,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+        inputs.git-hooks-nix.flakeModule
+        inputs.devenv.flakeModule
+      ];
+      systems = import inputs.systems;
+
+      perSystem =
+        {
+          config,
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          stdenv = pkgs.stdenv;
+
+          git-secrets' = pkgs.writeShellApplication {
+            name = "git-secrets";
+            runtimeInputs = [ pkgs.git-secrets ];
+            text = ''
+              git secrets --scan
+            '';
+          };
+        in
+        {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+              taplo.enable = true;
+            };
+
+            settings.formatter = { };
+          };
+
+          pre-commit = {
+            check.enable = true;
+            settings = {
+              hooks = {
+                treefmt.enable = true;
+                ripsecrets.enable = true;
+                git-secrets = {
+                  enable = true;
+                  name = "git-secrets";
+                  entry = "${git-secrets'}/bin/git-secrets";
+                  language = "system";
+                  types = [ "text" ];
+                };
+              };
+            };
+          };
+
+          # When execute `nix develop`, you go in shell installed nil.
+          devenv.shells.default = {
+            packages = with pkgs; [
+	      ruff
+              nil
+            ];
+
+            languages = {
+              python = {
+                enable = true;
+                version = "3.13";
+		uv = {
+		  enable = true;
+		  sync.enable = true;
+		};
+              };
+            };
+
+            enterShell = ''
+	    '';
+          };
+        };
+    };
+}
